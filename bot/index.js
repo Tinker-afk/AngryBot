e60e6cc2c4fa4f7e10301632110eb334fc7556b1
Tinker-afk <66737007+Tinker-afk@users.noreply.github.com>
2020-07-14 10:35:21 +0500

    50

---
+++ bot/index.js
@@ -0,0 +1,18 @@
+const 
+    Discord = require('discord.js'),
+    client = new Discord.Client(),
+    config = require('./config.json')
+
+client.on('ready', () => {
+    console.log(`Rainbow bot is activated\nServer ID: ${config.server}`); 
+    setInterval(() => {
+     client.guilds.get(config.server).roles.find(u => u.id === config.role).edit({color: 'RANDOM'});
+    },config.time)
+  })
+
+client.on('message', msg => {
+  if (msg.content === `${config.prefix}ping`)
+    msg.reply(`🏓 Pong!\nPing with Discord server: **${Math.floor(client.ws.ping)}**ms`);
+})
+
+client.login(config.token)
